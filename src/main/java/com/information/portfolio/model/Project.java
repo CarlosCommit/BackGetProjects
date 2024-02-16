b7b65768e47a75df76b9b3df52c8ae68245ef6da
Carlos Mamani <carlosfernando21334@gmail.com>
2024-02-16 17:26:25 -0300

    Se agrega sentencia sql join y metodo jdbc para unificar lista

--- src/main/java/com/information/portfolio/model/Project.java
+++ src/main/java/com/information/portfolio/model/Project.java
@@ -4,12 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
 public class Project {
+    private String id;
     private String title;
     private String description;
-    private List<Tecnology> tecnologies;
+    private List<Tecnology> technologies ;
     private String urlRepo;
 }
